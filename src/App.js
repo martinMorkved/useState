b51b0b0f997d78b0dae1fc175ae8aa30919de9e2
martinMorkved <38659462+martinMorkved@users.noreply.github.com>
2020-11-19 11:59:09 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -22,6 +22,32 @@ function decrease() {
 
 
     </div>
+
+
+
+/**function App() {
+  const now = new Date().toLocaleTimeString();
+
+  const [time, setTime] = useState(now);
+
+  function updateTime() {
+    const newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
+  }
+
+  setInterval(updateTime, 1000);
+
+  return (
+    <div className="container">
+      <h1>{time}</h1>
+      <button onClick={updateTime}>Get Time</button>
+    </div>
+  );
+} */
+
+
+
+
   );
 }
 
