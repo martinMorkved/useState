c648fa89c1e88ca5a371520c39b6cf7cce6aed08
martinMorkved <38659462+martinMorkved@users.noreply.github.com>
2020-11-19 11:30:34 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,25 +2,23 @@ import React, {useState} from "react";
 import './App.css';
 
 function App() {
-const [count , setCount] = useState(0);
 
+const [count, setCount] = useState(0);
 
 function increase() {
-  setCount(count + 1);
-}
-function decrease() {
-  setCount(count - 1);
+  setCount(count+1)
 }
 
+function decrease() {
+  setCount(count-1);
+}
 
 
   return (
     <div className="App">
-      <div className="container">
-      <h1>{count}</h1>
-      <button onClick= {increase}>+</button>
-      <button onClick= {decrease}>-</button>
-      </div>
+     <h1>{count}</h1>
+     <button onClick={increase}>+</button>
+     <button onClick={decrease}>-</button>
 
 
     </div>
